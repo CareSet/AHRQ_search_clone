8b25f64877e3f7b9bd7e84510fef9901267f5e8a
Fred Trotter <fred.trotter@gmail.com>
2018-07-13 21:19:47 -0500

    initial

---
+++ get_measures.php
@@ -0,0 +1,18 @@
+<?php
+
+
+	//there are 26 pages of results, starting with #1, as long as we get 100 results at once.  
+
+	for($page = 1; $page <= 27; $page++){
+
+		$url = "https://www.qualitymeasures.ahrq.gov/search?f_DocType=302&fLockTerm=Measure+Summaries&page=$page&pageSize=100";
+		echo "$url\n";
+		
+		$cmd = "wget --mirror --convert-links --adjust-extension --page-requisites --no-parent '$url'";
+
+	//	system($cmd);
+
+	}
+
+	
+
