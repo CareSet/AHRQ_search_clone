14610cb6c26ebdda5658974b1e882ac84e38bbad
Fred Trotter <fred.trotter@gmail.com>
2018-07-15 17:42:01 -0500

    initial

--- one_off_mirror/mirror.php
+++ one_off_mirror/mirror.php
@@ -1,4 +1,13 @@
 <?php
 
+	//just a quick way to call a longer wget command...
+	//wisdom from https://gist.github.com/dannguyen/03a10e850656577cfb57
 
+	if(!isset($argv[1])){
+		echo "Usage: php mirror.php {url_to_mirror}";
+		exit();
+	}
 
+	$url = $argv[1];
+
+	$cmd = "wget --adjust-extension --span-hosts --convert-links --backup-converted  --timestamping --page-requisites $url";
