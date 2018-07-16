85564e0787999e4bb3a749323da1d4b3155a127e
Fred Trotter <fred.trotter@gmail.com>
2018-07-15 20:12:14 -0500

    adding expert commentary

--- get_expert_commentary.php
+++ get_expert_commentary.php
@@ -3,39 +3,77 @@
 
 	$dir = "expert_commentary";
 
-	chdir("./$dir");
 
-
-	$expert_comments_page = "https://www.qualitymeasures.ahrq.gov/expert";
-
-	$doc = new DOMdocument;
-	//its messy html, so ignore the load errors... 
-	echo "Starting download...";
-	$html_text = file_get_contents($expert_comments_page);
-	echo "done. \nLoading DOM..";
-	@$doc->loadhtml($html_text);
-	$xpath = new DOMXPath($doc);
-	echo "done. \n";
+	//parse the following html files, looking for anchors with links
+	//matching the paried text.. then download the paired text. 
+	$worklist = [
+		'./one_off_mirror/www.guideline.gov/expert.html' => 'expert-commentary',
+		'./one_off_mirror/www.guideline.gov/syntheses/index.html' => 'synthesis',
+		'./one_off_mirror/www.qualitymeasures.ahrq.gov/expert.html' => 'expert-commentary',
+		];
 
 	$data = [];
+	foreach($worklist as $source_file => $filter){
+
+		$doc = new DOMdocument;
+		//its messy html, so ignore the load errors... 
+//		echo "Starting download...";
+		$html_text = file_get_contents($source_file);
+//		echo "done. \nLoading DOM..";
+		@$doc->loadhtml($html_text);
+		$xpath = new DOMXPath($doc);
+//		echo "done. \n";
+
 		
-	$xpath_query = '//a';
-	echo "Procesing matching anchors\n";
-	foreach($xpath->query($xpath_query) as $this_link){
-		$anchor = $this_link->nodeValue;
-		$href = $this_link->getAttribute('href');
-		echo "found $href\n";
-		if(strpos($href,'expert-commentary') !== false){
-			$tmp = [];
-			$tmp['anchor'] = $anchor;
-			$tmp['href'] = $href;
-			$data[] = $tmp;
+		$xpath_query = '//a';
+//		echo "Procesing matching anchors\n";
+		foreach($xpath->query($xpath_query) as $this_link){
+			$anchor = $this_link->nodeValue;
+			$href = $this_link->getAttribute('href');
+//			echo "found $href\n";
+			if(strpos($href,$filter) !== false){
+//				echo "This link matches our current filter:$filter!! so we are going to mirror it!!\n";
+				$tmp = [];
+				$tmp['anchor'] = $anchor;
+				$tmp['href'] = $href;
+				$data[] = $tmp;
+			}
 		}
+
 	}
 
-	//lets download the data from wayback machine..
-	
+	chdir("./$dir");
 	foreach($data as $i => $row){
+		$href = $row['href']; //now I have href..
+
+		$parsed = parse_url($href);
+		$domain_name = $parsed['host'];
+		$pathinfo = pathinfo($parsed['path']);
+		$dirname = $pathinfo['dirname'];
+		$basename = $pathinfo['basename'];
+		$file_to_test = "./$domain_name/$dirname/$basename.html";
+		
+		
+		echo "Looking for $file_to_test\n";
+
+		if(!file_exists($file_to_test)){
+			echo "Missing file\n";
+
+			$cmd = "wget --adjust-extension --span-hosts --convert-links --backup-converted  --timestamping --page-requisites $href";
+			system($cmd);
+			echo "
+###########################################################################################################
+###########################################################################################################
+FINISHED $href
+###########################################################################################################
+###########################################################################################################
+\n\n";
+
+
+		}else{
+			echo "Got file $file_to_test... moving right along...\n";
+		}
+
 	}
 
 
